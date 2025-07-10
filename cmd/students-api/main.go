1e27d0d799e8b3b3ac4594b4529edede91e9f855
Sonu Kumar <sonuhjpkumar@gmail.com>
2025-07-10 07:31:56 +0530

    Every Thing Setup data and Run Code Complete POstgreSql

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -13,39 +13,62 @@ import (
 
 	"github.com/sonu31/student-api/internal/config"
 	student "github.com/sonu31/student-api/internal/http/handlers"
-	"github.com/sonu31/student-api/internal/storage/sqlite"
+	postgressSql "github.com/sonu31/student-api/internal/storage/postgrsSql"
 )
 
 func main() {
 	fmt.Println("HI Go")
 	//load confg
 	cfg := config.MustLoad()
-	storage, sqlerr := sqlite.New(cfg)
-	if sqlerr != nil {
-		log.Fatal(sqlerr)
 
+	// storage, sqlerr := sqlite.New(cfg)
+	// if sqlerr != nil {
+	// 	log.Fatal(sqlerr)
+
+	// }
+	// slog.Info("Storage Initlialized ", slog.String("env", cfg.Env), slog.String("Verion", "1.0.0"))
+	// SQL
+	// router := http.NewServeMux()
+	// router.HandleFunc("POST /api/students", student.Create(storage))
+	// router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
+	// router.HandleFunc("GET /api/students", student.GetList(storage))
+	// router.HandleFunc("PUT /api/students/{id}", student.UpdateItme(storage))
+
+	postgreSql, pSqlerr := postgressSql.New(cfg)
+	if pSqlerr != nil {
+		log.Fatal(pSqlerr)
 	}
-	slog.Info("Storage Initlialized ", slog.String("env", cfg.Env), slog.String("Verion", "1.0.0"))
 
+	//PostgrSql
 	router := http.NewServeMux()
-	router.HandleFunc("POST /api/students", student.Create(storage))
-	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
-	router.HandleFunc("GET /api/students", student.GetList(storage))
-	router.HandleFunc("PUT /api/students/{id}", student.UpdateItme(storage))
+	router.HandleFunc("POST /api/students", student.Create(postgreSql))
+	router.HandleFunc("GET /api/students/{id}", student.GetById(postgreSql))
+	router.HandleFunc("GET /api/students", student.GetList(postgreSql))
+	router.HandleFunc("PUT /api/students/{id}", student.UpdateItme(postgreSql))
 
 	// database setup
 
 	//setup router
 
 	//setuo server
+	/* 	serve := http.Server{
+	   		Addr:    cfg.Adds,
+	   		Handler: router,
+	   	}
+	   	slog.Info("Server Start", slog.String("address", cfg.Adds))
+
+	   	done := make(chan os.Signal, 1)
+
+	   	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM) */
+
 	serve := http.Server{
-		Addr:    cfg.Adds,
+		Addr:    cfg.HTTPServer.Address,
 		Handler: router,
 	}
-	slog.Info("Server Start", slog.String("address", cfg.Adds))
+
+	slog.Info("Server Start", slog.String("address", cfg.HTTPServer.Address))
 
 	done := make(chan os.Signal, 1)
-
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
--- internal/config/config.go
+++ internal/config/config.go
@@ -8,18 +8,38 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
-type HTTPServer struct {
-	Adds string `yaml:"address" env-required:"true"`
-}
+// type HTTPServer struct {
+// 	Adds string `yaml:"address" env-required:"true"`
+// }
 
 // env - default:""production
-type Config struct {
-	Env         string `yaml:"env" env:"ENV" env-rrequired:"true"`
-	StoragePath string `yaml:"storage_path" env-required:"true"`
-	HTTPServer  `yaml:"http_server"`
+//
+//	type Config struct {
+//		Env         string `yaml:"env" env:"ENV" env-rrequired:"true"`
+//		StoragePath string `yaml:"storage_path" env-required:"true"`
+//		HTTPServer  `yaml:"http_server"`
+//	}
+type PostgresConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	DBName   string `yaml:"dbname"`
+	SSLMode  string `yaml:"sslmode"`
 }
 
-func MustLoad() *Config {
+type HTTPServer struct {
+	Address string `yaml:"address"`
+}
+
+type ConfigPSQL struct {
+	Env        string         `yaml:"env"`
+	Postgres   PostgresConfig `yaml:"postgres"`
+	HTTPServer HTTPServer     `yaml:"http_server"`
+}
+
+// changes Config to  ConfigPSQL
+func MustLoad() *ConfigPSQL {
 	var configPath string
 	configPath = os.Getenv("CONFIG_PATH")
 
@@ -39,8 +59,8 @@ func MustLoad() *Config {
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		log.Fatalf("Config file does not exits: %s ", configPath)
 	}
-
-	var cfg Config
+	// Change it
+	var cfg ConfigPSQL
 
 	err := cleanenv.ReadConfig(configPath, &cfg)
 	if err != nil {
